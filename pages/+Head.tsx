5e3a9235617c3a8d925126080186aed5e665ce95
yousifpa98 <yousif.paulus@dci-student.org>
2025-04-14 12:57:35 +0200

    satsu tracking

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -6,6 +6,7 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={logoUrl} />
+      <script src="https://cdn.satsu.pro/tracker.js" data-site="6XQGkAIUxx"></script>
     </>
   );
 }
